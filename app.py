c4a0e2adae9d11d4c4a3abea583f00e9608d3881
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-18 12:58:31 +0200

    adjusted files to fit with the mobilenet model

--- app.py
+++ app.py
@@ -6,22 +6,25 @@ import os
 
 app = Flask(__name__)
 
-model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg19', pretrained=True)
+model = torch.onnx.load('mobilenet.onnx')
+
+def preprocess_image(image_path):
+    image = Image.open(image_path)
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    image = transform(image).unsqueeze(0)
+    return image
 
 def predict(image_path):
     try:
-        # image preprocessing
-        image = Image.open(image_path)
-        transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
-        ])
-        image = transform(image).unsqueeze(0)
+        image = preprocess_image(image_path)
         with torch.no_grad():
             output = model(image)
             _, predicted = torch.max(output, 1)
@@ -29,15 +32,12 @@ def predict(image_path):
             with open('imagenet_classes.txt') as f:
                 classes = [line.strip() for line in f.readlines()]
                 prediction = classes[class_index]
-                prediction = prediction.split(",")[1].strip().capitalize()  # Get the class name and capitalize the first letter
+                prediction = prediction.split(",")[1].strip().capitalize()
             return prediction
     except Exception as e:
         error_msg = "Error: Failed to preprocess or classify the image. Please ensure the image is in a supported format and try again."
         print(f"{error_msg}\n{e}")
         return error_msg
-
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -54,7 +54,6 @@ def index():
         uploaded_image = None
         prediction = None
     return render_template('index.html', prediction=prediction, image=uploaded_image)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
